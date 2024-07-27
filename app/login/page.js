b226fa0da5aa6d8ec3093f4cacd9db30a2f154d4
Ignacio Herrera <ignacioromanherrera@gmail.com>
2024-07-27 01:08:26 -0300

    Login page

--- app/login/page.js
+++ app/login/page.js
@@ -1,39 +1,35 @@
 "use client";
 import { useState } from "react";
 import toast from "react-hot-toast";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
+import { signIn } from "next-auth/react";
 
-export default function Register() {
-  const [name, setName] = useState("");
+export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
 
   const router = useRouter();
+  const searchParams = useSearchParams();
+  const callbackUrl = searchParams.get("callbackUrl") ?? "/";
 
   const handleSubmit = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
     try {
       setLoading(true);
-      // Todo move to admin action and use process.env.API_URL
-      const resp = await fetch(`http://localhost:3000/api/register`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          name,
-          email,
-          password,
-        }),
-      });
-      const data = await resp.json();
 
-      if (!resp.ok) {
-        toast.error(data.err);
+      const result = await signIn("credentials", {
+        email,
+        password,
+        redirect: false,
+      });
+
+      if (result?.error) {
+        toast.error(result.error);
       } else {
-        toast.success(data.success);
-        router.push("/login");
+        toast.success("Login exitoso");
+        // TODO redirect to dashboard if no callbackUrl
+        router.push(callbackUrl);
       }
     } catch (err) {
       toast.error(err.message);
@@ -46,25 +42,9 @@ export default function Register() {
     <main>
       <div className="space-y-12 mt-1">
         <div className="border-b border-gray-900/10 pb-12">
-          <h2 className="text-base font-semibold leading-7 text-gray-900 text-center">Register</h2>
+          <h2 className="text-base font-semibold leading-7 text-gray-900 text-center">Login</h2>
 
           <form onSubmit={handleSubmit}>
-            <div className="grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6 p-5">
-              <div className="sm:col-span-4">
-                <label htmlFor="name" className="block text-sm font-medium leading-6 text-gray-900">Name</label>
-                <div className="mt-2">
-                  <div
-                    className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                    <input type="text" name="name" id="name" autoComplete="name"
-                           className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                           value={name}
-                           onChange={(e) => setName(e.target.value)}
-                           placeholder="Enter your full name"/>
-                  </div>
-                </div>
-              </div>
-            </div>
-
             <div className="grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6 p-5">
               <div className="sm:col-span-4">
                 <label htmlFor="name" className="block text-sm font-medium leading-6 text-gray-900">Email</label>
@@ -106,8 +86,17 @@ export default function Register() {
               </button>
             </div>
           </form>
+
+          <div className="mt-6 ml-6">
+            <button type="submit"
+                    className="rounded-md bg-amber-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    onClick={() => signIn("google", {callbackUrl})}
+            >
+              Ingresar con Google
+            </button>
+          </div>
         </div>
       </div>
     </main>
-  )
+  );
 }
