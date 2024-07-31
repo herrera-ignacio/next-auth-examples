54676b98ca1cc74bbecfd41afe761e823679d57e
Ignacio Herrera <ignacioromanherrera@gmail.com>
2024-07-30 23:22:35 -0300

    Remove console.logs

--- app/api/admin/route.js
+++ app/api/admin/route.js
@@ -1,15 +1,7 @@
 import { NextResponse } from "next/server";
 import { authHandler } from "@/app/api/authHandler";
-import {auth} from "@/utils/auth";
 
-// export const GET = authHandler(function GET(req) {
-//   const message = "Hello from the admin-only API!"
-//   return NextResponse.json({ message });
-// }, "admin");
-
-
-export const GET = auth(function GET(req) {
-  console.log(req.auth);
+export const GET = authHandler(function GET(req) {
   const message = "Hello from the admin-only API!"
   return NextResponse.json({ message });
-});
+}, "admin");
--- app/api/authHandler.js
+++ app/api/authHandler.js
@@ -7,11 +7,9 @@ import { auth } from "@/utils/auth";
  * @param {string} [role]
  */
 export const authHandler = (routeHandler, role) => auth((req => {
-  console.log("REQ AUTH", req.auth);
   if (!req.auth) return NextResponse.json({ message: "Not authenticated" }, { status: 401 });
 
   if (role && req.auth?.user?.role !== role) {
-    console.log("FAILS AUTHORIZATION")
     return NextResponse.json({ message: "Forbidden" }, { status: 403 });
   }
 
